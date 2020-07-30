efe1abc6db9c27d67830db46cb87a0a79f36d6a6
Caitlin Clary <caitlin.clary@biostatglobal.com>
2020-07-30 12:39:34 -0400

    Updated opplot demo, with data

--- opplot_demo.R
+++ opplot_demo.R
@@ -1,116 +1,163 @@
 ####################################################################################
-# Program:     demo_opplot.r
+# Program:     opplot_demo.r
 # Purpose:     Demonstrate making organ pipe plots
 # Author:      Mary Prier, Biostat Global Consulting
+#              Caitlin Clary, Biostat Global Consulting
 # Delivered:   2017-01-28
 # Updated:     2019-03-26
+#              2020-07-30       Demo with updated function
+# 
 # Input Data:  testdata_indiv_level.csv or testdata_indiv_level_stratum_numeric.csv
-# Required Functions: opplot (source it in)
+# Required Functions: opplot.R
 # Required Packages: doBy
 ####################################################################################
 
-rm(list=ls())
+# Get needed packages if not already installed
+if("doBy" %in% rownames(installed.packages()) == FALSE){
+        install.packages("doBy")}
 
-#install.packages("doBy")
-library(doBy) # If this lines errors, uncomment & run the install package doBy first (previous line)
-source('Q:/Papers - Organ Pipe Plots/R programs/opplot.R')
+# Load required packages 
+library(doBy) 
 
-# Set file paths
-inPath <- file.path("Q:/Papers - Organ Pipe Plots/Test data")
-outPath <- file.path("Q:/Papers - Organ Pipe Plots/R output")
+# Source the opplot.R function 
+# Change this file path to point to the program's location on your computer
+source('C:/Documents/opplot.R')
+
+# Set file paths: 
+  # inPath = directory holding the data 
+  # outPath = directory for saved output 
+inPath <- file.path("C:/Documents/Data")
+outPath <- file.path("C:/Documents/Output")
 
 # Read in data
-inData <- read.csv(file=file.path(inPath,"testdata_indiv_level.csv"),header=T)
+inData <- read.csv(file = file.path(inPath,"testdata_indiv_level.csv"), header = TRUE)
 
-# Example 1: All options listed (default outputs plot to screen...plot NOT saved to disk)
-#            Note: option "plotn=TRUE" for this example, but it's default is "plotn=FALSE"
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       barcolor1="hot pink", barcolor2="white",
-       linecolor1="gray", linecolor2="gray",
-       ylabel="Percent of Cluster", 
-       ymin=0, ymax=100, yby=50,
-       title="This is the title line", subtitle="This is the subtitle line", footnote="Footnote",
-       output_to_screen=T, filename=file.path(outPath,"test2"),
-       platform="pdf", sizew=7, sizeh=6, savedata="opplot_stratumA_dataset",
-       plotn=TRUE, nlinecolor="black", nlinewidth=1, nlinepattern=2,
-       ytitle2="Number of Respondents", yround2=5)
+## Plotting Examples 
 
-# Let's break it down...
+# Example 1 ---- 
+# All function options are listed, default appearance options selected 
+# Plot to screen, do not save plot to disk 
 
-# Example 2: Minimum inputs for this dataset with multiple strata (specifies stratvar & stratum)
-#            Note: Outputs plot to screen...plot NOT saved to disk
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-        weightvar="svyweight", yvar="y", stratum="Stratum A")
+opplot(dat = inData, stratvar = "stratum", clustvar = "clusterid",
+       weightvar = "svyweight", yvar = "y", stratum = "Stratum A",
+       barcolor1 = "hot pink", barcolor2 = "white",
+       linecolor1 = "gray", linecolor2 = "gray",
+       ylabel = "Percent of Cluster", 
+       ymin = 0, ymax = 100, yby = 50,
+       title = "This is the title line", 
+       subtitle = "This is the subtitle line", 
+       footnote = "Footnote",
+       output_to_screen = TRUE, filename = file.path(outPath, "test2"),
+       platform = "pdf", sizew = 7, sizeh = 6, savedata = "",
+       plotn = FALSE, nlinecolor = "black", nlinewidth = 1, nlinepattern = 2,
+       ytitle2 = "Number of Respondents", yround2 = 5)
 
-# Example 3: Save plot as .wmf (plot not output to screen)
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       title="This is the title line",
-       output_to_screen=F, filename=file.path(outPath,"stratumA"),
-       platform="wmf")
+# Example 2 ----
+# Same as example 1, but with sample size plotted (plotn = TRUE)
 
-# Example 4: Save plot as .pdf (plot not output to screen)
-#            & save dataset of the opplot (includes 1 row per bar in the plot)
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       title="This is the title line",
-       output_to_screen=F, filename=file.path(outPath,"stratumA"),
-       platform="pdf", savedata="opplot_stratumA_dataset")
+opplot(dat = inData, stratvar = "stratum", clustvar = "clusterid",
+       weightvar = "svyweight", yvar = "y", stratum = "Stratum A",
+       barcolor1 = "hot pink", barcolor2 = "white",
+       linecolor1 = "gray", linecolor2 = "gray",
+       ylabel = "Percent of Cluster", 
+       ymin = 0, ymax = 100, yby = 50,
+       title = "This is the title line", 
+       subtitle = "This is the subtitle line", 
+       footnote = "Footnote",
+       output_to_screen = TRUE, filename = file.path(outPath, "test2"),
+       platform = "pdf", sizew = 7, sizeh = 6, savedata = "",
+       plotn = TRUE, nlinecolor = "black", nlinewidth = 1, nlinepattern = 2,
+       ytitle2 = "Number of Respondents", yround2 = 5)
 
-# Example 5: Save plot as .png (plot not output to screen)
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       title="This is the title line",
-       output_to_screen=F, filename=file.path(outPath,"stratumA"),
-       platform="png")
+# Example 3 ---- 
+# Minimal example, plotting all strata (because of the number of clusters in the example data, this plot isn't very readable)
 
-# Example 6: Output plot to screen & change bar colors & add titles
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       barcolor1="blue", barcolor2="gray",
-       linecolor1="black", linecolor2="black", 
-       ylabel="This is the y-axis title line", title="This is the title line", 
-       subtitle="This is the subtitle line", footnote="This is the footnote line")
+opplot(dat = inData, clustvar = "clusterid", yvar = "y")
 
-# Example 7: Output plot to screen & change plot aspect ratio & y-axis increments
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       title="Test Data", yby=20,
-       output_to_screen=T, sizew=5, sizeh=5)
+# Example 4 ----
+# Minimal example, plotting a single stratum 
 
-# Example 8: Output plot to screen & add number of respondents line
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       title="This is the title line", output_to_screen=T, 
-       plotn=T)
+opplot(dat = inData, clustvar = "clusterid", yvar = "y", 
+       stratvar = "stratum", stratum = "Stratum A")
 
-# Example 9: Output plot to screen & add number of respondents line & change its appearance
-opplot(dat=inData, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum="Stratum A",
-       title="This is the title line", output_to_screen=T, 
-       plotn=T, nlinecolor="green", nlinewidth=3, nlinepattern=3,
-       ytitle2="Number of Respondents", yround2=2)
+# Example 5 ----
+# Saving PDF plot to disk
 
-# Example 10: Using a dataset with only one stratum
-#            Output plot to screen & "stratvar" & "stratum" not provided 
-# First, subset dataset to only one stratum...
-inData2 <- inData[inData$stratum=="Stratum A",]
+opplot(dat = inData, clustvar = "clusterid", yvar = "y", 
+       stratvar = "stratum", stratum = "Stratum B", 
+       filename = "StratumB", platform = "pdf",
+       output_to_screen = FALSE)
 
-# Now "stratvar" & "stratum" do not need to be specified
-opplot(dat=inData2, clustvar="clusterid",
-        weightvar="svyweight", yvar="y",
-        title="This is the title line",
-        output_to_screen=T)
+# Example 6 ----
+# Saving plot to disk as a PNG & changing some appearance options
 
-# Example 11: Output plot to screen & stratum variable is numeric
-#             In this case, the "stratum" input is now "1" or 1 (as opposed to "Stratum A")
-#             Or could call stratum="2" or stratum=2 or so on...
-inData3 <- read.csv(file=file.path(inPath,"testdata_indiv_level_stratum_numeric.csv"),header=T)
-opplot(dat=inData3, stratvar="stratum", clustvar="clusterid",
-       weightvar="svyweight", yvar="y", stratum=1,
-       title="This is the title line",
-       output_to_screen=T)
+opplot(dat = inData, clustvar = "clusterid", yvar = "y", weightvar = "svyweight",
+       stratvar = "stratum", stratum = "Stratum B", 
+       filename = "StratumB", platform = "png",
+       output_to_screen = FALSE,
+       plotn = TRUE, nlinecolor = "black", nlinewidth = 1, nlinepattern = 2,
+       barcolor1 = "#004709",
+       barcolor2 = "#e1e1e1",
+       title = "Test Title: Stratum B")
+
+# Example 7 ----
+# Changing bar and line colors, line pattern
+opplot(dat = inData, clustvar = "clusterid", yvar = "y", weightvar = "svyweight",
+       stratvar = "stratum", stratum = "Stratum C", 
+       output_to_screen = TRUE,
+       plotn = TRUE, nlinecolor = "grey20", nlinewidth = 1, nlinepattern = 3,
+       barcolor1 = "lightcoral",
+       barcolor2 = "floralwhite",
+       linecolor1 = "white", linecolor2 = "white",
+       title = "Stratum C")
+
+# Example 8 ----
+# Changing the plot dimensions
+opplot(dat = inData, clustvar = "clusterid", yvar = "y", weightvar = "svyweight",
+       stratvar = "stratum", stratum = "Stratum D", 
+       output_to_screen = TRUE,
+       plotn = TRUE, nlinecolor = "grey20", nlinewidth = 1, nlinepattern = 3,
+       sizew = 9, sizeh = 6,
+       barcolor1 = "lightcoral",
+       barcolor2 = "white",
+       linecolor1 = "white", linecolor2 = "white",
+       title = "Stratum D")
+
+# Example 9 ----
+# Instead of providing stratvar and stratum arguments, instead 
+# providing a filtered dataset with only the stratum of interest. 
+
+subset_data <- inData[inData$stratum == "Stratum D",]
+
+opplot(dat = subset_data, clustvar = "clusterid", yvar = "y", weightvar = "svyweight",
+       output_to_screen = TRUE,
+       plotn = TRUE, nlinecolor = "grey20", nlinewidth = 1, nlinepattern = 3,
+       sizew = 9, sizeh = 6,
+       barcolor1 = "lightcoral",
+       barcolor2 = "white",
+       linecolor1 = "white", linecolor2 = "white",
+       title = "Stratum D")
+
+# Example 10 ----
+# Specifying the savedata argument, which creates a .csv dataset
+# with information on each bar (cluster) in the plot. 
+# In this example the plot is neither saved nor output to the screen
+
+opplot(dat = inData, clustvar = "clusterid", yvar = "y", weightvar = "svyweight",
+       stratvar = "stratum", stratum = "Stratum D", 
+       output_to_screen = FALSE,
+       savedata = "Stratum_D_data")
+
+# Read in the dataset just saved  
+strat_D <- read.csv("Stratum_D_data.csv")
+
+# View the dataset 
+View(strat_D)
+
+# End of demo ----
 
 # Close all graphics printed to the screen
 graphics.off()
+
+# Remove data and other objects 
+rm(list = "inPath", "outPath", "inData", "subset_data", "strat_D")
